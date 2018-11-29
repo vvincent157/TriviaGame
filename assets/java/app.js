91df77094111ac6a07e3a363eba2b9582c97a403
Victoria <Victoria@Victorias-MacBook-Pro.local>
2018-11-29 10:02:00 -0600

    added updated changes to HW

--- assets/java/app.js
+++ assets/java/app.js
@@ -77,8 +77,11 @@ var game = {
  },
   // this function starts our game
   start: function() {
+      this.counter = 120;
+      $('#quiz-area').empty();
     // sets the interval on the coundown at 1 second at a time.
     timer = setInterval(game.countdown, 1000);
+
 
     // Display the countdown timer
 	$("#sub-wrapper").prepend("<h2> Time Remaining <span id = 'counter-number'>120</span></h2>");
@@ -86,21 +89,24 @@ var game = {
     $("#start").remove();
     
     for(var i =0; i < questions.length; i++){
-        card.append("<h2>" + questions[i].question + "</h2>");
+        $('#quiz-area').append("<h2>" + questions[i].question + "</h2>");
         for (var r = 0; r < questions[i].answers.length; r++){
-            $('#quiz-form').append(` <label for='question-${i}'><b>${questions[i].question}</b></label>`);
-            $('#quiz-form').append(`<input type='radio' name='question-${i}' value="${questions[i].answers}">${questions[i].answers[r]}<br>`);
+            // $('#quiz-area').append(` <label for='question-${i}'><b>${questions[i].question}</b></label>`);
+            $('#quiz-area').append(`<input type='radio' name='question-${i}' value="${questions[i].answers}">${questions[i].answers[r]}<br>`);
             // if(i === 0) {
-            //     $('#quiz-form').append(` <label for='question-${i}'><b>${questions[i].question}</b></label>`);
+            //     $('#quiz-area').append(` <label for='question-${i}'><b>${questions[i].question}</b></label>`);
 
             // }
         }
     
   
     }
+    $('#quiz-area').append(`<button id= 'done'> Done</button>`);
 	// loop over the questions and answers
 	  // this is a loop within a loop...
-      $('#quiz-form').append(`<input type="submit" id="done" value="Submit">`);
+    //   $('#quiz-area').append(`<input type="submit" id="done" value="Submit">`);
+
+     
 	// make sure to have a done button append at the bottom here.
   },
 
@@ -109,7 +115,7 @@ var game = {
     // loops over each input and responds to the check.
     
     $.each(
-        $("input[name= 'question-0]: checked"), function(){
+        $("input[name='question-0']:checked"), function(){
             if($(this).val()=== questions[0].correctAnswer){
                 game.correct++;
             }
@@ -117,7 +123,7 @@ var game = {
             }
         });
     $.each(
-        $("input[name= 'question-1]: checked"), function(){
+        $("input[name='question-1']:checked"), function(){
             if($(this).val()=== questions[1].correctAnswer){
                 game.correct++;
             }
@@ -125,7 +131,7 @@ var game = {
             }
         });
     $.each(
-        $("input[name= 'question-2]: checked"), function(){
+        $("input[name='question-2']:checked"), function(){
             if($(this).val()=== questions[2].correctAnswer){
                 game.correct++;
             }
@@ -133,7 +139,7 @@ var game = {
             }
         });
     $.each(
-        $("input[name= 'question-3]: checked"), function(){
+        $("input[name='question-3']:checked"), function(){
             if($(this).val()=== questions[3].correctAnswer){
                 game.correct++;
             }
@@ -141,7 +147,7 @@ var game = {
             }
         });  
     $.each(
-    $("input[name= 'question-4]: checked"), function(){
+    $("input[name='question-4']:checked"), function(){
         if($(this).val()=== questions[4].correctAnswer){
             game.correct++;
         }
@@ -149,7 +155,7 @@ var game = {
         }
     });   
     $.each(
-        $("input[name= 'question-5]: checked"), function(){
+        $("input[name='question-5']:checked"), function(){
             if($(this).val()=== questions[5].correctAnswer){
                 game.correct++;
             }
@@ -157,7 +163,7 @@ var game = {
             }
         });   
     $.each(
-        $("input[name= 'question-6]: checked"), function(){
+        $("input[name='question-6']:checked"), function(){
             if($(this).val()=== questions[6].correctAnswer){
                 game.correct++;
             }
@@ -165,7 +171,7 @@ var game = {
             }
         });   
     $.each(
-        $("input[name= 'question-7]: checked"), function(){
+        $("input[name='question-7']:checked"), function(){
             if($(this).val()=== questions[7].correctAnswer){
                 game.correct++;
             }
@@ -173,7 +179,7 @@ var game = {
             }
         });   
     $.each(
-        $("input[name= 'question-8]: checked"), function(){
+        $("input[name='question-8']:checked"), function(){
             if($(this).val()=== questions[8].correctAnswer){
                 game.correct++;
             }
@@ -181,7 +187,7 @@ var game = {
             }
         });
     $.each(
-        $("input[name= 'question-9]: checked"), function(){
+        $("input[name='question-9']:checked"), function(){
             if($(this).val()=== questions[9].correctAnswer){
                 game.correct++;
             }
@@ -189,7 +195,7 @@ var game = {
             }
         });
         $.each(
-            $("input[name= 'question-10]: checked"), function(){
+            $("input[name='question-10']:checked"), function(){
                 if($(this).val()=== questions[10].correctAnswer){
                     game.correct++;
                 }
@@ -207,17 +213,19 @@ var game = {
 
 // this function sets the results after you click the done button
   result: function() {
-  // clear the timer as the game is over....  
+      console.log('hi')
+//   clear the timer as the game is over....  
     clearInterval(timer);
-	 // remove the questions and done button
+	//  remove the questions and done button
     // display the results of the game
     $("#sub-wrapper h2").remove();
 
-    card.html("<h2>You're Done!</h2>");
-    card.append("<h3> Correct Answers:" + this.correct + "</h3>");
-    card.append("<h3> Incorrect Answers:" + this.incorrect + "</h3>");
-    card.append("<h3> Unanswered:" + (questions.length - (this.incorrect + this.correct)) + "</h3>");
-  }
+    $('#quiz-area').html("<h2>You're Done!</h2>");
+    $('#quiz-area').append("<h3> Correct Answers:" + this.correct + "</h3>");
+    $('#quiz-area').append("<h3> Incorrect Answers:" + this.incorrect + "</h3>");
+    $('#quiz-area').append("<h3> Unanswered:" + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+    $('#quiz-area').append("<button id='start'> Try Again </button>");
+    }
 };
 
 // CLICK EVENTS
@@ -226,6 +234,8 @@ $(document).on("click", "#start", function() {
     game.start();
  // make sure to run the staet fucntion here, reference the game object
 });
+
+
 
 
 $(document).on("click", "#done", function() {
